dc29697a2c3969191ca5e055f31539dcdb481bc5
Nurudeen Amedu <nurudeen.amedu@gmail.com>
2021-07-09 12:47:09 +0100

    added base64 image use

---
+++ src/Base64Image.js
@@ -0,0 +1,36 @@
+import { useState } from "react"
+
+function Base64Image(){
+    const [base64, setBase64] = useState("")
+
+    const onChange = (e) => {
+        console.log("file to upload:", e.target.files[0])
+        let file= e.target.files[0]
+
+        if(file){
+            const reader = new FileReader()
+            reader.onload = function handleReaderLoaded(event){
+                let binaryString = event.target.result
+                setBase64(btoa(binaryString))
+            }
+            reader.readAsBinaryString(file)
+        }
+    }
+
+    return(
+        <>
+            <form onChange={(e) => onChange(e)}>
+                <input 
+                    type="file"
+                    name="image"
+                    id="file"
+                    accept=".jpeg, .png, .jpg"
+                />
+                <input type="submit" />
+            </form>
+            {base64.length>1 && <img src={`data:image/png;base64,${base64}`} alt="a to b" />}
+        </>
+    )
+}
+
+export default Base64Image
